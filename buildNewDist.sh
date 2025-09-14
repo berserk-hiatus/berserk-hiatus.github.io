30152b39bcf87c2e867b0d108acd64edd1a5e0d0
berserk-hiatus <rafz7@protonmail.com>
2025-09-14 18:45:01 -0300

    Updated bash file to also update the master branch when gh-pages's dist is updated.

--- buildNewDist.sh
+++ buildNewDist.sh
@@ -5,14 +5,17 @@ if [ $# -gt 1 ]; then
   exit 1
 fi
 
-if [ -z $1 ]
+if [ -z "$1" ]
 then
 	echo "ERROR: Write a commit message!"
     exit 1
 fi
 
-#npm run build
-git add .
+# Build dist and add its changes to gh-pages branch
+npm run build
+git add dist -f
 git commit -m "$1"
-#git subtree push --prefix dist origin gh-pages
+git subtree push --prefix dist origin gh-pages
+
+# Update master branch with file changes
 git push
